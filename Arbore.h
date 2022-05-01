42dad37c3f9ab384c0229a103ee1d24a4328e0a9
Raul Alexandru Sandu <100997266+Rasaalpw@users.noreply.github.com>
2022-05-01 23:06:28 +0300

    Add files via upload

---
+++ Arbore.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class Arbore
+{
+public:
+	struct Node
+	{
+		int val;
+		Node* st;
+		Node* dr;
+	};
+	int nr_noduri;
+	Arbore();
+	Arbore(const Arbore&);
+	~Arbore();
+	Node* radacina;
+	Node* AdaugareFrunza(int val);
+	int GetNrNod();
+	void ParcurgerePre(Node* a);
+	void ParcurgereIn(Node* a);
+	void ParcurgerePost(Node* a);
+	int DetNivel(Node*, int, int);
+	void AfisareNoduriNivel(Node* a, int, int);
+	int Inaltime(Node*);
+	void AfisareFrunze();
+	void InsertNode(int x, Node* a);
+	friend istream& operator>>(istream& is, Arbore&);
+	friend ostream& operator<<(ostream& os, Arbore&);
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Arbore.h"
+#include "Node.h"
+using namespace std;
+int main()
+{
+	Arbore A;
+	A.InsertNode(10, A.radacina);
+	cout << A;
+	
+
+}
